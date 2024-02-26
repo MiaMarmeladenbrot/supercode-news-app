a3aac5ebd63902a0cba537d02cf5a057dbcaed33
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-02-26 13:30:14 +0100

    Funktionen fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,54 @@
-// 1. output-Values festlegen
-// 2. Funktion, um search input in API-Link zu setzen
-// 3. Funktion, um nach Sprache zu filtern
-// 4. Funktion, um Ergebnisse zu sortieren
-// 5. EventListener auf submit-Button, um alle Ergebnisse im HTML auszugeben
+// # noch to do
+// - ternary, falls etwas keinen Inhalt hat
+// - CSS
+// - in weitere Funktionen aufteilen?
 
 const searchOutput = document.querySelector(".search-output");
 const form = document.querySelector("form");
 
-// ! Funktion, um Searchinput des Users zu bekommen und an API weiterzugeben:
+// ! Funktion, um mit Search-Input, Sprachauwahl und Sortierung API-Daten zu fetchen:
 const getUserInput = () => {
+  // * Input-Values:
   const userInput = document.querySelector("#search-input").value;
   console.log(userInput);
 
-  // * API Daten mit Searchinput des Users fetchen:
-  fetch(`https://newsapi.org/v2/everything?q=${userInput}&apiKey=01995892cbe34ab2ba4a69c3a6f542c1
+  const langInput = document.querySelector("#lang").value;
+  console.log(langInput);
+
+  const sortInput = document.querySelector("#sort").value;
+  console.log(sortInput);
+
+  // * API Daten mit Searchinput, Language-Auswahl und Sortierung fetchen:
+  fetch(`https://newsapi.org/v2/everything?q=${userInput}&language=${langInput}&sortBy${sortInput}&apiKey=01995892cbe34ab2ba4a69c3a6f542c1
   `)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((newsData) => {
+      console.log(newsData);
+
+      // * über alle Artikel des Suchergebnisses iterieren, um die jeweiligen Daten ins HTML zu schreiben:
+      newsData.articles.forEach((articleData) => {
+        console.log(articleData);
+        searchOutput.innerHTML += `
+        <div>
+        <h2>${articleData.title}</h2>
+        <p>${articleData.description}</p>
+        <img src="${articleData.urlToImage}" alt="${articleData.title}"/>
+        <a href="${articleData.url}" target="_blank">Zum Artikel</a>
+
+        </div>
+        `;
+      });
+    })
     .catch((err) => console.log(err));
 };
-
-// ! Funktion, um nach Sprache zu filtern
 
 // ! EventListener auf der Form/dem Submit-Button, um alles im HTML auszugeben:
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("test");
+  //   console.log("test");
+  // * HTML-Output-Feld vor jeder Such ausgabe leeren:
+  searchOutput.innerHTML = "";
 
+  // * Funktion für User-Such-Ausgabe aufrufen:
   getUserInput();
 });
